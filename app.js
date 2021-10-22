4ad29bd1bea79f829021aa73c36aa23317ca096a
choirul <choirul@ap.surabaya.go.id>
2021-10-22 09:33:26 +0700

    feat: add axios, api for pokemon list and single pokemon resolve #8, resolve #7

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("static", express.static(path.join(__dirname, "public")));
 app.use(cors());
--- routes/pokedex.js
+++ routes/pokedex.js
@@ -2,13 +2,17 @@ const express = require("express");
 const router = express.Router();
 const { authCheck, auth } = require("../libs/firebase");
 
+const axios = require("axios").create({
+  baseURL: "https://pokeapi.co/api/v2",
+  timeout: 1000,
+});
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.json({ title: "Pokedex" });
 });
-
 // auth register
-router.post("/auth/register", authCheck, async (req, res, next) => {
+https: router.post("/auth/register", authCheck, async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const data = await auth.createUser({ email, password });
@@ -25,6 +29,40 @@ router.post("/auth/register", authCheck, async (req, res, next) => {
 
 router.get("/protected", authCheck, function (req, res, next) {
   res.json({ title: "Your are authenticated user" });
+});
+
+// from https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+// api get one pokemon by pokedex id or name
+// pokeapi doest expose detail of pokemon, so we try to get all information one by one
+
+router.get("/pokemon", authCheck, async (req, res, next) => {
+  const { limit, offset } = req.query;
+  let result = {};
+  try {
+    result = await axios.get(`/pokemon?limit=${limit}&offset=${offset}`);
+
+    await asyncForEach(result.data.results, async (poke) => {
+      const detail = await axios.get(poke.url);
+      poke.detail = detail.data;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  return res.json(result.data);
+});
+
+router.get("/pokemon/:id", authCheck, async (req, res, next) => {
+  const id = parseInt(req.params.id);
+  const result = await axios.get(`/pokemon/${id}`);
+
+  return res.json(result.data);
 });
 
 module.exports = router;
