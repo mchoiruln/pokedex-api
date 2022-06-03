a034766ecc1f0270f4924e68069b1759afaead70
mchoiruln <mchoiruln@gmail.com>
2022-06-03 07:26:48 +0700

    feat: add express-rate-limit

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require("express-rate-limit");
+var limiter = new RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
+});
+
+// apply rate limiter to all requests
 const indexRouter = require("./routes/index");
 const pokedexRouter = require("./routes/pokedex");
 const authRouter = require("./routes/auth");
@@ -11,6 +19,7 @@ const authRouter = require("./routes/auth");
 const app = express();
 
 app.use(logger("dev"));
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
